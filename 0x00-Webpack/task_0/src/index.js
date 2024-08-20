e392a109df6cf5847086e09763b0a787c6fb6894
sw-ouahmane <133534816+sw-ouahmane@users.noreply.github.com>
2024-08-20 12:07:28 +0100

    Add

--- 0x00-Webpack/task_0/src/index.js
+++
@@ -1,6 +0,0 @@
-// Import jQuery
-import $ from 'jquery';
-
-$('body').append('<p>Holberton Dashboard</p>');
-$('body').append('<p>Dashboard data for the students</p>');
-$('body').append('<p>Copyright - Holberton School</p>');
